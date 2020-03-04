7b9e4334e6e57daec647ce8c93bc1d530a844616
KaiserBh <41852205+kaiserbh@users.noreply.github.com>
2020-03-04 11:44:22 +1100

    I have split Author and Contributor section and added the rest of Contributor.

--- PlexAniSync.py
+++ PlexAniSync.py
@@ -391,7 +391,8 @@ if __name__ == '__main__':
 
     print("""
     #########################################################################
-    # Author:   RickDB, KaiserBh                                            #
+    # Author:   RickDB                                                      #
+    # Contributor:  KaiserBh, WoLpH, Versatile-BFG, wiiaboo                 #
     # URL:      https://github.com/RickDB/PlexAniSync                       #
     # --                                                                    #
     #########################################################################
