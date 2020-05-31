68ea10f47d76d08c5e5972f00bbe3dda3523c4c5
Rick <RickDB@gmail.com>
2020-06-01 00:10:44 +0200

    Added missing continue for AniList results without year attribute

--- anilist.py
+++ anilist.py
@@ -1173,7 +1173,8 @@ def find_id_season_best_match(title, season, year):
                         if media_item.startDate.year is not None:
                             started_year = int(media_item.startDate.year)
                     else:
-                        logger.warning('[ANILIST] Anilist series did not have year attribute so skipping this result and moving to next: %s | %s' % (title_english, title_romaji ))   
+                        logger.warning('[ANILIST] Anilist series did not have year attribute so skipping this result and moving to next: %s | %s' % (title_english, title_romaji ))
+                        continue
 
                     for potential_title in potential_titles:
                         potential_title = re.sub(
