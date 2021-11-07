fa4748a8f6286565bb995a353d0df5e701f19b0d
reconman <reconman@users.noreply.github.com>
2021-11-07 16:29:48 +0100

    Fix pipelines

--- Docker/Tautulli/run/settingsupdater.py
+++ Docker/Tautulli/run/settingsupdater.py
@@ -4,19 +4,19 @@ import os
 config = configparser.ConfigParser()
 
 config['PLEX'] = {
-	'anime_section': os.environ.get('PLEX_SECTION'),
-	'authentication_method': 'direct',
-	'base_url': os.environ.get('PLEX_URL'),
-	'token': os.environ.get('PLEX_TOKEN'),
+    'anime_section': os.environ.get('PLEX_SECTION'),
+    'authentication_method': 'direct',
+    'base_url': os.environ.get('PLEX_URL'),
+    'token': os.environ.get('PLEX_TOKEN'),
 }
 
 config['ANILIST'] = {
-	'username': os.environ.get('ANI_USERNAME'),
-	'access_token': os.environ.get('ANI_TOKEN'),
-	'plex_episode_count_priority': os.environ.get('PLEX_EPISODE_COUNT_PRIORITY', False),
-	'skip_list_update': os.environ.get('SKIP_LIST_UPDATE', False),
-	'log_failed_matches': os.environ.get('LOG_FAILED_MATCHES', False),
+    'username': os.environ.get('ANI_USERNAME'),
+    'access_token': os.environ.get('ANI_TOKEN'),
+    'plex_episode_count_priority': os.environ.get('PLEX_EPISODE_COUNT_PRIORITY', False),
+    'skip_list_update': os.environ.get('SKIP_LIST_UPDATE', False),
+    'log_failed_matches': os.environ.get('LOG_FAILED_MATCHES', False),
 }
 
-with open('/plexanisync/settings.ini', 'w') as configfile:
-	config.write(configfile)
+with open('/plexanisync/settings.ini', 'w', encoding="UTF-8") as configfile:
+    config.write(configfile)
