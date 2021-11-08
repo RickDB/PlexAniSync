f528c122a14006486f633915fa43abb7e0f06ac9
reconman <reconman@users.noreply.github.com>
2021-11-08 23:02:40 +0100

    Revert "Switch container from alpine to slim-buster"
    This reverts commit 7ffa2f9d3976e69d56a74c14f60e8f86b4208016.

--- Docker/PlexAniSync/run/update.sh
+++ Docker/PlexAniSync/run/update.sh
@@ -7,6 +7,6 @@ cp -f /PlexAniSync-master/*.py /plexanisync
 cp -f /PlexAniSync-master/requirements.txt /plexanisync/requirements.txt
 rm -rf /PlexAniSync-master
 cd /plexanisync &&\
-python -m pip install -r requirements.txt &&\
+python3 -m pip install -r requirements.txt &&\
 cd ..
 echo "Update completed successfully"
