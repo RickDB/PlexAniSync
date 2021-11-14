068ba6bdfe8ac53e5fd2e142ed2816068f75b12d
KaiserBh <kaiserbh@protonmail.com>
2021-11-14 18:03:04 +1100

    added chagnes to run settingsupdater.py when env is not set

--- Docker/Tautulli/run/start.sh
+++ Docker/Tautulli/run/start.sh
@@ -10,8 +10,11 @@ usermod -o -u "$PUID" tautulli
 chown -R tautulli:tautulli /config
 chown -R tautulli:tautulli /plexanisync
 
-echo "Updating settings.ini"
-gosu tautulli "python" "/plexanisync/settingsupdater.py"
-
-echo "Running Tautulli using user tautulli (uid=$(id -u tautulli)) and group tautulli (gid=$(id -g tautulli))"
-exec gosu tautulli "$@"
+if [[ -z "${SETTINGS_FILE}" ]]; then
+  echo "Updating settings.ini"
+  gosu tautulli "python" "/plexanisync/settingsupdater.py"
+  exec gosu tautulli "$@"
+else
+  echo "Running Tautulli using user tautulli (uid=$(id -u tautulli)) and group tautulli (gid=$(id -g tautulli))"
+  exec gosu tautulli "$@"
+fi
