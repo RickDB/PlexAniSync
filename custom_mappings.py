a6fcbcb3eea5904076e3130c6ad5c667e25c99b4
KaiserBh <KaiserBh@protonmail.com>
2021-12-22 11:58:47 +1100

    changed now to work with one custom_mappings.yaml

--- custom_mappings.py
+++ custom_mappings.py
@@ -22,8 +22,6 @@ class AnilistCustomMapping:
 
 
 def read_custom_mappings():
-    remote_custom_mapping = get_custom_mapping_web()
-
     custom_mappings = {}
     if not os.path.isfile(MAPPING_FILE):
         logger.info(f"[MAPPING] Custom map file not found: {MAPPING_FILE}")
@@ -31,17 +29,15 @@ def read_custom_mappings():
     else:
         logger.info(f"[MAPPING] Custom mapping found locally, using: {MAPPING_FILE}")
 
+    remote_custom_mapping = get_custom_mapping_remote()
+
     schema = yamale.make_schema('./custom_mappings_schema.yaml', parser='ruamel')
 
     # Create a Data object
     file_mappings_local = yamale.make_data(MAPPING_FILE, parser='ruamel')
-
     try:
         # Validate data against the schema same as before.
         yamale.validate(schema, file_mappings_local)
-        if remote_custom_mapping is not None:
-            file_mappings_remote = yamale.make_data(content=remote_custom_mapping, parser='ruamel')
-            yamale.validate(schema, file_mappings_remote)
     except YamaleError as e:
         logger.error('Custom Mappings validation failed!\n')
         for result in e.results:
@@ -49,36 +45,52 @@ def read_custom_mappings():
                 logger.error(f"{error}\n")
         sys.exit(1)
     if remote_custom_mapping is not None:
-        for file_entry in file_mappings_local[0][0]['entries']:
-            for file_entry_remote in file_mappings_remote[0][0]['entries']:
-                series_mappings: List[AnilistCustomMapping] = []
-                if file_entry['title'] == file_entry_remote['title']:
-                    logger.info(f"[MAPPING] Custom mapping found duplicate..using {MAPPING_FILE} instead: {file_entry['title']}")
-                    series_title = str(file_entry['title'])
-                    synonyms: List[str] = file_entry.get('synonyms', [])
-                    for file_season in file_entry['seasons']:
-                        season = file_season['season']
-                        anilist_id = file_season['anilist-id']
-                        start = file_season.get('start', 1)
-                        logger.info(
-                            f"[MAPPING-LOCAL] Adding custom mapping | title: {series_title} | season: {season} | anilist id: {anilist_id} | start: {start}"
-                        )
-                        series_mappings.append(AnilistCustomMapping(season, anilist_id, start))
-                else:
-                    series_title = str(file_entry_remote['title'])
-                    synonyms: List[str] = file_entry_remote.get('synonyms', [])
-                    for file_season_remote in file_entry_remote['seasons']:
-                        season = file_season_remote['season']
-                        anilist_id = file_season_remote['anilist-id']
-                        start = file_season_remote.get('start', 1)
-                        logger.info(
-                            f"[MAPPING-REMOTE] Adding custom mapping | title: {series_title} | season: {season} | anilist id: {anilist_id} | start: {start}"
-                        )
-                        series_mappings.append(AnilistCustomMapping(season, anilist_id, start))
+        # loop through list tuple
+        for index, tuple in enumerate(remote_custom_mapping):
+            mapping_location = tuple[0]
+            yaml_content = tuple[1]
+            try:
+                file_mappings_remote = yamale.make_data(content=yaml_content, parser='ruamel')
+                yamale.validate(schema, file_mappings_remote)
+            except YamaleError as e:
+                logger.error(f'Custom Mappings {mapping_location} validation failed!\n')
+                for result in e.results:
+                    for error in result.errors:
+                        logger.error(f"{error}\n")
+                sys.exit(1)
 
-                custom_mappings[series_title.lower()] = series_mappings
-                for synonym in synonyms:
-                    custom_mappings[synonym.lower()] = series_mappings
+            for file_entry in file_mappings_local[0][0]['entries']:
+                for file_entry_remote in file_mappings_remote[0][0]['entries']:
+                    series_mappings: List[AnilistCustomMapping] = []
+                    if file_entry['title'] == file_entry_remote['title']:
+                        logger.info(f"[MAPPING] Custom mapping found duplicate: {file_entry['title']} using {MAPPING_FILE} instead: {mapping_location}")
+                        series_title = str(file_entry['title'])
+                        synonyms: List[str] = file_entry.get('synonyms', [])
+                        for file_season in file_entry['seasons']:
+                            season = file_season['season']
+                            anilist_id = file_season['anilist-id']
+                            start = file_season.get('start', 1)
+                            logger.info(
+                                f"[MAPPING] Adding custom mapping from {MAPPING_FILE}"
+                                f"| title: {series_title} | season: {season} | anilist id: {anilist_id} | start: {start}"
+                            )
+                            series_mappings.append(AnilistCustomMapping(season, anilist_id, start))
+                    else:
+                        series_title = str(file_entry_remote['title'])
+                        synonyms: List[str] = file_entry_remote.get('synonyms', [])
+                        for file_season_remote in file_entry_remote['seasons']:
+                            season = file_season_remote['season']
+                            anilist_id = file_season_remote['anilist-id']
+                            start = file_season_remote.get('start', 1)
+                            logger.info(
+                                f"[MAPPING] Adding custom mapping from {mapping_location}"
+                                f"| title: {series_title} | season: {season} | anilist id: {anilist_id} | start: {start}"
+                            )
+                            series_mappings.append(AnilistCustomMapping(season, anilist_id, start))
+
+                    custom_mappings[series_title.lower()] = series_mappings
+                    for synonym in synonyms:
+                        custom_mappings[synonym.lower()] = series_mappings
         return custom_mappings
     else:
         for file_entry in file_mappings_local[0][0]['entries']:
@@ -91,7 +103,8 @@ def read_custom_mappings():
                 start = file_season.get('start', 1)
 
                 logger.info(
-                    f"[MAPPING-LOCAL] Adding custom mapping | title: {series_title} | season: {season} | anilist id: {anilist_id} | start: {start}"
+                    f"[MAPPING] Adding custom mapping from {MAPPING_FILE}"
+                    f"| title: {series_title} | season: {season} | anilist id: {anilist_id} | start: {start}"
                 )
                 series_mappings.append(AnilistCustomMapping(season, anilist_id, start))
 
@@ -102,20 +115,20 @@ def read_custom_mappings():
 
 
 # Get the custom mappings from web then checks if the file is valid first.
-def get_custom_mapping_web():
-    if not os.path.isfile(REMOTE_MAPPING_FILE):
-        logger.info(f"[MAPPING-REMOTE] Remote mapping: {REMOTE_MAPPING_FILE} file not found, skipping")
+def get_custom_mapping_remote():
+    if not os.path.isfile(MAPPING_FILE):
+        logger.info(f"[MAPPING] {MAPPING_FILE} remote urls found, skipping")
         return None
     else:
-        logger.info(f"[MAPPING-REMOTE] Remote mapping: {REMOTE_MAPPING_FILE} found, using")
+        logger.info(f"[MAPPING] {MAPPING_FILE} found, using")
         remote_mappings_urls = []
         custom_mappings_remote = []
-        custom_mappings_web = ''
+        custom_mappings_remote_file_name = []
 
-        schema = yamale.make_schema('./remote_mappings_schema.yaml', parser='ruamel')
+        schema = yamale.make_schema('./custom_mappings_schema.yaml', parser='ruamel')
 
         # Create a Data object
-        file_mappings = yamale.make_data(REMOTE_MAPPING_FILE, parser='ruamel')
+        file_mappings = yamale.make_data(MAPPING_FILE, parser='ruamel')
 
         try:
             # Validate data against the schema same as before.
@@ -125,31 +138,35 @@ def get_custom_mapping_web():
             for result in e.results:
                 for error in result.errors:
                     logger.error(f"{error}\n")
-            logger.error("[MAPPING-REMOTE] Remote Mappings validation failed!")
+            logger.error("[MAPPING] Remote Mappings validation failed!")
             sys.exit(1)
 
-        # loop through the remote mappings and add urls to the list
-        for file_entry in file_mappings[0][0]['remotes']:
-            remote_map_name = str(file_entry['name'])
-            urls: List[str] = file_entry.get('urls', [])
-            # append urls to  list remote_mappings
-            for url in urls:
+        try:
+            # loop through the remote mappings and add urls to the list
+            for url in file_mappings[0][0]['remote-urls']:
+                # append urls to  list remote_mappings
                 remote_mappings_urls.append(url)
-                logger.info(f"[MAPPING-REMOTE] Adding remote mapping: {remote_map_name} | url: {url}")
+                logger.info(f"[MAPPING] Adding remote mapping url: {url}")
 
-        # Get url and read the data
-        for urls in remote_mappings_urls:
-            response = requests.get(urls)
-            if response.status_code == 200:
-                custom_mappings_remote.append(response.text)
+            # check if the array is not empty
+            if not remote_mappings_urls:
+                logger.info(f"[MAPPING] No remote mapping urls found in {MAPPING_FILE}")
+                return None
+            # split url and get the file name
+            for url in remote_mappings_urls:
+                custom_mappings_remote_file_name.append(url.split('/')[-1])
+                logger.info(f"[MAPPING] Adding remote mapping file name: {url.split('/')[-1]}")
 
-        # join the data from the list into one string
-        custom_mappings_web = '\n'.join(custom_mappings_remote)
+            # Get url and read the data
+            for urls in remote_mappings_urls:
+                response = requests.get(urls)
+                if response.status_code == 200:
+                    custom_mappings_remote.append(response.text)
 
-        # remove enteries key from string but leave the first one.
-        custom_mappings_web = custom_mappings_web.replace('\nentries:', '\n')
+            # create tuple of file name and data
+            custom_mappings_remote = list(zip(custom_mappings_remote_file_name, custom_mappings_remote))
 
-        # add the first enteries key back to the string
-        custom_mappings_web = 'entries:\n' + custom_mappings_web
-
-        return custom_mappings_web
+            return custom_mappings_remote
+        except (KeyError, TypeError):
+            logger.info(f"[MAPPING] No remote mapping urls found in {MAPPING_FILE}")
+            return None
