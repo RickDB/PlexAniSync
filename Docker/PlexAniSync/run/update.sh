31776ae2a6c5ad0534f0b382fa0f6e3a196c06d5
reconman <reconman@users.noreply.github.com>
2023-03-20 20:25:47 +0100

    Deal with empty environment variables
    Fixes #185

--- Docker/PlexAniSync/run/update.sh
+++ Docker/PlexAniSync/run/update.sh
@@ -4,6 +4,7 @@ wget https://github.com/RickDB/PlexAniSync/archive/master.zip &&\
 unzip master.zip &&\
 rm master.zip &&\
 cp -f /PlexAniSync-master/*.py /plexanisync
+cp -rf /PlexAniSync-master/plexanisync /plexanisync/plexanisync
 cp -f /PlexAniSync-master/requirements.txt /plexanisync/requirements.txt
 rm -rf /PlexAniSync-master
 cd /plexanisync &&\
