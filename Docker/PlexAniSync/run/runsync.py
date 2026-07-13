1cee4d77db26b7148fe23bbb93c68220ea5e862e
reconman <reconman@users.noreply.github.com>
2026-07-13 23:16:45 +0200

    Switch to distroless for main image (#300)

---
+++ Docker/PlexAniSync/run/runsync.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+import sys
+import time
+
+PLEXANISYNC_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def run():
+    while True:
+        subprocess.run([sys.executable, os.path.join(PLEXANISYNC_DIR, "PlexAniSync.py")])
+        interval = int(os.environ.get("INTERVAL", 0))
+        if interval > 0:
+            time.sleep(interval)
+        else:
+            print("Sync was completed and INTERVAL <= 0, quitting")
+            break
+
+
+if __name__ == "__main__":
+    if not os.environ.get("SETTINGS_FILE"):
+        print("Updating settings.ini")
+        subprocess.run(
+            [sys.executable, os.path.join(PLEXANISYNC_DIR, "settingsupdater.py")], check=True
+        )
+    else:
+        print("Using custom config: " + os.environ["SETTINGS_FILE"])
+    run()
